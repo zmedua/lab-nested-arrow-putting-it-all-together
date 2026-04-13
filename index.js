a8389729408310f21734f3058a1e6a9f27c12eaf
Zane <zimedua@gmail.com>
2026-04-13 18:00:35 -0500

    lab nested functions

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+function createLoginTracker(userInfo) 
+{
+let attemptCount = 0;  
+    
+  return function (password) 
+    { if (attemptCount >= 3) {
+        
+        return "Account locked due to too many failed login attempts";
+    }
+      if (password === userInfo.password) 
+      { attemptCount = 0; 
+        return "Login successful";
+    
+      } 
+    else 
+    {attemptCount++;
+      return `Attempt ${attemptCount}: Login failed`; 
+
+    }
+    }
+  
+};
 
 
 
